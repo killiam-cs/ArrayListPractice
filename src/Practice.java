8f1b9c59221f0fd2d7e381fc66b2ee4806f4b1a2
killiam-cs <charlotte_killiam@milton.edu>
2023-03-31 11:31:39 -0400

    Initial commit

---
+++ src/Practice.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Practice {
+
+    ArrayList<Integer> numbers;
+
+    public static void main(String[] args) {
+        Practice p = new Practice();
+    }
+
+    public Practice() {
+        // construct numbers arraylist with integers from 0 to 15
+
+    }
+
+}
